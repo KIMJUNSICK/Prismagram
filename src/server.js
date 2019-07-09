246b5feae509c42b70e042c8ffb19c6807fdd8c7
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-09 16:48:07 +0900

    Creating GraphQL Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log("junsik");
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query{
+        hello :String!
+    }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "junsik"
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start({ port: PORT }, () =>
+  console.log(`Server running on ✅  http://localhost:${PORT}`)
+);
