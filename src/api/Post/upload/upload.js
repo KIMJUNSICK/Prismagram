b7cdd9563ef23f900be2a21a3e65e83bcc743bd3
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-15 22:32:00 +0900

    upload Resolver

---
+++ src/api/Post/upload/upload.js
@@ -0,0 +1,26 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    upload: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { caption, files } = args;
+      const post = await prisma.createPost({
+        caption,
+        user: { connect: { id: user.id } }
+      });
+      files.forEach(async file =>
+        prisma.createFile({
+          url: file,
+          post: {
+            connect: {
+              id: post.id
+            }
+          }
+        })
+      );
+      return post;
+    }
+  }
+};
