8d96f34de7f72deca29c87b0041957b6a9160815
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-19 16:50:06 +0900

    newMessage Subscription

---
+++ src/api/Message/newMessage/newMessage.js
@@ -0,0 +1,28 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Subscription: {
+    newMessage: {
+      subscribe: (_, args) => {
+        const { roomId } = args;
+        return prisma.$subscribe
+          .message({
+            AND: [
+              {
+                mutation_in: "CREATED"
+              },
+              {
+                node: {
+                  room: {
+                    id: roomId
+                  }
+                }
+              }
+            ]
+          })
+          .node();
+      },
+      resolve: payload => payload
+    }
+  }
+};
