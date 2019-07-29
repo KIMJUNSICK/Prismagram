19f08642c52943be2e87537cb630fec10de5f218
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-29 21:05:01 +0900

    Revise computedUser

--- src/api/User/computedUser.js
+++ src/api/User/computedUser.js
@@ -19,7 +19,7 @@ export default {
         .count(),
     followersCount: ({ id }) =>
       prisma
-        .usersConnection({ where: { following_none: { id } } })
+        .usersConnection({ where: { following_some: { id } } })
         .aggregate()
         .count(),
     fullName: parent => {
